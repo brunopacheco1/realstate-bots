76ab267583fb8d499efd605395c3cdf53d2dfb99
Bruno Pacheco <brunopacheco1@yahoo.com>
2020-11-10 02:21:18 +0100

    Removing not used constant.

--- realstate-backend/src/main/java/com/github/brunopacheco1/realstate/consumers/PubSubConstants.java
+++ realstate-backend/src/main/java/com/github/brunopacheco1/realstate/consumers/PubSubConstants.java
@@ -1,8 +1,6 @@
 package com.github.brunopacheco1.realstate.consumers;
 
 public class PubSubConstants {
-    public static final String UELZECHT_CRAWLER = "uelzecht-crawler";
-    public static final String ATHOME_CRAWLER = "athome-crawler";
     public static final String SENDING_NOTIFICATION = "notification-sending";
     public static final String INCOMING_PROPERTY = "property-incoming";
     public static final String FINDING_PROPERTY = "property-finding";
